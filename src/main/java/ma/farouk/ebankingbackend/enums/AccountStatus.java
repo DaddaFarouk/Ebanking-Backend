6518879dadb0b1260c81ca78eec62da473fd81c7
DaddaFarouk <faroukdadda@gmail.com>
2022-05-15 23:25:48 +0100

    Creating classes

---
+++ src/main/java/ma/farouk/ebankingbackend/enums/AccountStatus.java
@@ -0,0 +1,5 @@
+package ma.farouk.ebankingbackend.enums;
+
+public enum AccountStatus {
+    CREATED, ACTIVATED, SUSPENDED
+}
---
+++ src/main/java/ma/farouk/ebankingbackend/enums/OperationType.java
@@ -0,0 +1,5 @@
+package ma.farouk.ebankingbackend.enums;
+
+public enum OperationType {
+    DEBIT, CREDIT
+}
