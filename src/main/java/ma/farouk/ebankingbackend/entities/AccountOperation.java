1743d91bec678cd2b5b7138b6be5f9e92312ec47
Farouk Dadda <DADDA_FAROUK@emsi-edu.ma>
2022-05-16 14:09:39 +0100

    second day

--- src/main/java/ma/farouk/ebankingbackend/entities/AccountOperation.java
+++ src/main/java/ma/farouk/ebankingbackend/entities/AccountOperation.java
@@ -5,15 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ma.farouk.ebankingbackend.enums.OperationType;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date operationDate;
     private double amount;
     private OperationType type;
+    @ManyToOne
     private BankAccount bankAccount;
 }
--- src/main/java/ma/farouk/ebankingbackend/entities/BankAccount.java
+++ src/main/java/ma/farouk/ebankingbackend/entities/BankAccount.java
@@ -5,16 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ma.farouk.ebankingbackend.enums.AccountStatus;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+@Entity
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date createdAt;
     private AccountStatus status;
+    @ManyToOne
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperation> accountOperations;
 }
